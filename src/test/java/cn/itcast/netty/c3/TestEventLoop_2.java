55fabae16b13b677f5248ea5b00bb85086001b02
r23456r <r23456r@163.com>
2021-04-26 22:19:06 +0800

    2021年4月26日 22:19:03

--- src/test/java/cn/itcast/netty/c3/TestEventLoop_2.java
+++ src/test/java/cn/itcast/netty/c3/TestEventLoop_2.java
@@ -4,12 +4,14 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.TimeUnit;
+
 /**
- * 普通任务
+ * 定时任务
  */
 
 @Slf4j
-public class TestEventLoop_1 {
+public class TestEventLoop_2 {
     public static void main(String[] args) {
         EventLoopGroup group = new NioEventLoopGroup(2); //IO事件，
 //        EventLoopGroup group = new DefaultEventLoopGroup(); //普通任务和定时任务
@@ -20,14 +22,10 @@ public class TestEventLoop_1 {
         System.out.println(group.next());
 
         //执行普通任务 这明显是个异步操作哦~~~~
-        group.next().submit(() -> {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            log.info("ok");
-        });
+        group.next().scheduleAtFixedRate(() -> {
+
+            log.info("我被定时执行了");
+        }, 1, 1, TimeUnit.SECONDS);//延迟一秒后，一秒的间隔执行
         log.info("main先执行？？ 牛逼");
     }
 }
